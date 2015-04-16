d975972ad8dc16cea14a1c2ebd121a0100c4db39
ericbls <eric.lee.santos@gmail.com>
2015-04-16 09:40:23 -0300

    Revert "Nutrição_EP3"
    This reverts commit f81d0b97b1395a4449e87b9d20f4f8ccf0167cdd.

--- Nutrição_EP3.py
+++ Nutrição_EP3.py
@@ -7,12 +7,14 @@ arquivo_usuario = open('usuario.csv','r')
 arquivo_alimentos = open('alimentos.csv','r')
 
 # arquivo que conterá o índice de massa corporal do usuário e a indicação se este está saudável
-arquivo_resultado = open('resultado.csv','r+')
+arquivo_resultado = open('resultado.txt','r+')
 
 
 usuario = arquivo_usuario.readlines()
 
-def IMC(massa,altura):
+print (usuario)
+
+def IDM(massa,altura):
     return massa/altura**2
     
 def calcula_harris(altura,massa,idade,sexo):
@@ -21,13 +23,9 @@ def calcula_harris(altura,massa,idade,sexo):
     else:
         return 88.36+(13.4*massa)+(4.8*altura)-(5.7*idade)
 
-# passa as informações do usuário do arquivo para um dicioário
+# passo intermediário para transformar as informações do usuário em dicionário
 lista_usuario = usuario[1].split(',')
-dict_usuario = dict()
-dict_usuario[lista_usuario[0]] = [lista_usuario[1],lista_usuario[2],lista_usuario[3],lista_usuario[4],lista_usuario[5]]
 
-lista_alimentos = arquivo_alimentos.readlines()
-dict_alimento = dict()
-for i in range(1,len(lista_alimentos)):
-    linha_alimento = lista_alimentos[i].split(',')
-    dict_alimento[linha_alimento[0]] = [linha_alimento[1],linha_alimento[2],linha_alimento[3],linha_alimento[4],linha_alimento[5]]
+# adicionando as informações do usuário num dicionário
+info_usuario = dict()
+info_usuario[lista_usuario[0]] = [lista_usuario[1],lista_usuario[2],lista_usuario[3],lista_usuario[4],lista_usuario[5]]
