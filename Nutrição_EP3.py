e82a8053ba79b6ab0a536161506348bcfb918f0a
ericbls <eric.lee.santos@gmail.com>
2015-04-22 23:34:50 -0300

    Nutrição_EP3
    Este trabalho está se demonstrando demasiadamente tediante e chato.

--- Nutrição_EP3.py
+++ Nutrição_EP3.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-'''
-Importando arquivos e funções para o programa
-'''
+''' Importa os arquivos e funções usadas nesta programação '''
 
 arquivo_usuario = open('usuario.csv','r+')
 
@@ -10,17 +8,17 @@ arquivo_alimentos = open('alimentos.csv','r')
 
 from funcao_nutricao import *
 
+
 '''
 Criará uma lista com o nome do usuário e as informações fisícas associadas a ele
 '''
-
 lista_usuario = arquivo_usuario.readlines()                                    # Lista com todas as informações do usuário
 descricao_usuario = lista_usuario[1].split(',')                                # Lista temporaria contendo as informações da 2° linha do arquivo "usuarios.csv"
+
 
 '''
 Criará um dicionário com todos os  alimentos e as informações associadas a eles
 '''
-
 dict_alimentos = dict()		                                             # Dicionário que conterá todas as informações dos alimentos
 lista_alimentos = arquivo_alimentos.readlines()
 for i in range(1, len(lista_alimentos)):
@@ -31,7 +29,6 @@ for i in range(1, len(lista_alimentos)):
 '''
 Criará um dicionário com as informações nutricionais do usuário (o que ele consumiu em cada dia considerado)
 '''
-
 dict_ingestao = dict()                                                         # Dicionário com as informações daquilo que o usuário ingeriu por dia
 lista_ingestao = list()                                                        # Uma lista em que cada elemento é uma lista com outros 3 elementos (a data, o alimento e a quantidade)
 datas = list()                                                                 # Lista com todas as datas consideradas
@@ -50,11 +47,12 @@ for x in datas:                                                                #
         if x == z[0]:                                                          # Estabelece uma condição: somente realizará a função descrita acima se a data em que o alimento foi consumido for igual a data da chave no dicionário
             dict_ingestao[x].append([z[1],z[2]])
             
+
 '''
 Criará um dicionário com as informações de quantas calorias (em Kcal) e quanto de proteínas, carboidratos e gorduras (em gramas) ele consumiu em cada dia
 '''
-
 dict_dadosnutri = dict()                                                       # Diciánario com os valores nutricionais consumidos a cada dia (calorias, proteínas, carboidratos,. Exemplo: {'07/04/15': [1553.50, 36.53, 223.57, 57.07], '06/04/15': [462.50, 7.22, 62.85, 20.25]}
+
 
 for a in datas: 
     cal = 0
@@ -62,23 +60,22 @@ for a in datas:
     carb = 0
     gord = 0
     for b in range(len(dict_ingestao[a])):
-        prop = float("{0:.2f}".format(dict_ingestao[a][b][1]))/float("{0:.2f}".format(dict_alimentos[dict_ingestao[a][b][0]][0]))   # fração de quanto o usuário consumiu de uma porção do alimento (quantidade no arquivo "alimentos.csv").
+        prop = float(dict_ingestao[a][b][1])/float(dict_alimentos[dict_ingestao[a][b][0]][0])   # fração de quanto o usuário consumiu de uma porção do alimento (quantidade no arquivo "alimentos.csv").
                                                                                                 # Dict_ingestao[a][b][1] equivale a porção ([1]) consumida de um alimento [b] no dia [a]
                                                                                                 # Dict_alimentos[dict_ingestao[a][b][0]][0] equivale a porção usada como base para os valores nutricionais do "alimentos.csv"       
-        cal = cal + float("{0:.2f}".format(((dict_alimentos[dict_ingestao[a][b][0]][1])*prop))    # Adiciona a quantidade de calorias consumidas no alimento à quantidade de calorias já consumida no dia
+        cal = cal + (float(dict_alimentos[dict_ingestao[a][b][0]][1])*prop)    # Adiciona a quantidade de calorias consumidas no alimento à quantidade de calorias já consumida no dia
                                                                                # float(dict_alimentos[dict_ingestao[a][b][0]][1]) equivale a quantidade de calorias que uma porção do alimento [b] possui        
-        prot = prot + float("{0:.2f}".format(((dict_alimentos[dict_ingestao[a][b][0]][2])*prop))  # Adiciona a quantidade de proteínas consumidas no alimento à quantidade de proteínas já consumida no dia
+        prot = prot + (float(dict_alimentos[dict_ingestao[a][b][0]][2])*prop)  # Adiciona a quantidade de proteínas consumidas no alimento à quantidade de proteínas já consumida no dia
                                                                                # float(dict_alimentos[dict_ingestao[a][b][0]][3]) equivale a quantidade de proteínas que uma porção do alimento [b] possui           
-        carb = carb + float("{0:.2f}".format(((dict_alimentos[dict_ingestao[a][b][0]][3])*prop))  # Adiciona a quantidade de carboidratos consumidas no alimento à quantidade de carboidratos já consumida no dia
+        carb = carb + (float(dict_alimentos[dict_ingestao[a][b][0]][3])*prop)  # Adiciona a quantidade de carboidratos consumidas no alimento à quantidade de carboidratos já consumida no dia
                                                                                # float(dict_alimentos[dict_ingestao[a][b][0]][3]) equivale a quantidade de carboidratos que uma porção do alimento [b] possui     
-        gord = gord + float("{0:.2f}".format(((dict_alimentos[dict_ingestao[a][b][0]][4])*prop))  # Adiciona a quantidade de gorduras consumidas no alimento à quantidade de gorduras já consumida no dia
+        gord = gord + (float(dict_alimentos[dict_ingestao[a][b][0]][4])*prop)  # Adiciona a quantidade de gorduras consumidas no alimento à quantidade de gorduras já consumida no dia
                                                                                # float(dict_alimentos[dict_ingestao[a][b][0]][4]) equivale a quantidade de gorduras que uma porção do alimento [b] possui   
     dict_dadosnutri[a] = [cal,prot,carb,gord]                                  # Coloca o valor total de calorias, proteínas, carboidratos e gorduras consumidas no dia no dicionário (como um valor), associado a uma chave, que será a data (o dia em que tudo foi consumido)
 
 '''
 Criará uma lista com as datas consideradas, só que em ordem cronológica
 '''
-
 datas_crono = sorted(datas)
 
 '''
@@ -88,22 +85,28 @@ Criará uma lista com a quantidade calórica que usuário deveria consumir por d
 
 calorias_necessarias = [0]*len(datas_crono)
 for g in range(len(calorias_necessarias)):
-    calorias_necessarias[g] = calcula_harris(float(descricao_usuario[1]),float(descricao_usuario[2]),float(descricao_usuario[3]),float(descricao_usuario[4]),float(descricao_usuario[5]))
+    calorias_necessarias[g] = calcula_harris(float(descricao_usuario[1]),float(descricao_usuario[2]),descricao_usuario[3],float(descricao_usuario[4]),descricao_usuario[5])
+
 
 '''
 Escreverá as informações necessárias no arquivo "resultado.csv"
 '''
-
 resultado = open('resultado.txt','w',encoding='utf8')
+
 resultado.write('Abaixo estão o seu índice de massa corporal (IDM), a quantidade de calorias que você consumiu\ne o quanto foi consumido a mais ou a menos segundo a fórmula de Harris-Benedict em cada dia e,\npor fim, se você está subnutrido, saudável, possui sobrepeso, ou obesidade de graus I, II ou III\n\n')
+
 imc = IMC(float(descricao_usuario[2]),float(descricao_usuario[4]))
+
 calorias_recomendadas = calcula_harris(float(descricao_usuario[1]),float(descricao_usuario[2]),descricao_usuario[3],float(descricao_usuario[4]),descricao_usuario[5])
+
 resultado.write('Dia, calorias consumidas e quantas calorias você consumiu a mais ou a menos do que deveria:\n\n')
+
 for h in datas_crono:
     calorias_consumidas = dict_dadosnutri[h][0]
     resultado.write('dia %s   '%h)
     resultado.write('calorias consumidas: %f   '%calorias_consumidas)
     resultado.write('calorias consumidas a mais/menos: %f\n\n'%(calorias_consumidas-calorias_recomendadas))
+
 if imc<18.5:
     resultado.write('Você está subnutrido, segundo o seu IMC (calculado através de sua massa e altura)\n')
 elif imc>=18.5 and imc<=24.9:
@@ -117,17 +120,67 @@ elif imc>=35 and imc<=39.9:
 else:
     resultado.write('Você possui um sério caso de obesidade, segundo o seu IMC (calculado através de sua massa e altura)\n')
 
+
 '''
 Plotará os gráficos das calorias
 '''
+
+import matplotlib.pyplot as plt
+import numpy as np
     
-grafico_calorias(datas_crono,dict_dadosnutri[a][0],calorias_necessarias)
+calorias_consumidas = []
+for a in datas_crono:
+    calorias_consumidas.append(dict_dadosnutri[a][0])
+     
+todos_elementos = calorias_consumidas + calorias_necessarias
+     
+y = calorias_consumidas
+k = calorias_necessarias
+    
+ax = plt.subplot(111)
+ax.bar(np.arange(0,len(datas_crono))+0.8, y,width=0.2,color='b')
+ax.bar(np.arange(0,len(datas_crono))+1.0, k,width=0.2,color='r')
+ax.set_xticks(np.arange(len(datas_crono))+1)
+ax.set_xticklabels(datas_crono)
+plt.axis([0,len(datas_crono)+1,0,max(todos_elementos)+200])
+legenda="Calorias ingeridas(Kcal)","Calorias recomendadas(Kcal)"
+plt.legend(legenda,bbox_to_anchor=(1.0, -0.15),ncol=2,fancybox=True, shadow=True)
+plt.show()
+
 
 ''' 
 Plotará os gráficos do consumo dos outros nutrientes
 '''
 
-grafico_nutricional(datas_crono,dict_dadosnutri[a][1],dict_dadosnutri[a][2],dict_dadosnutri[a][3])
+import matplotlib.pyplot as plt
+import numpy as np
+    
+proteinas = list()
+carboidratos = list()
+gorduras = list()
+
+for a in datas_crono:
+    proteinas.append(dict_dadosnutri[a][1])
+    carboidratos.append(dict_dadosnutri[a][2])
+    gorduras.append(dict_dadosnutri[a][3])        
+
+todos_elementos = proteinas + carboidratos + gorduras    
+    
+y = proteinas
+z = carboidratos
+k = gorduras
+    
+ax = plt.subplot(111)
+ax.bar(np.arange(0,len(datas_crono))+0.7, y,width=0.2,color='b')
+ax.bar(np.arange(0,len(datas_crono))+0.9, z,width=0.2,color='g')
+ax.bar(np.arange(0,len(datas_crono))+1.1, k,width=0.2,color='r')
+ax.set_xticks(np.arange(len(datas_crono))+1)
+ax.set_xticklabels(datas_crono)
+plt.axis([0,len(datas_crono)+1,0,max(todos_elementos)+5])
+legendas="Proteínas","Carboidratos","Gorduras"
+plt.legend(legendas,bbox_to_anchor=(1.0, -0.15),ncol=2,fancybox=True, shadow=True)
+plt.show()
+
 
 arquivo_alimentos.close
 arquivo_usuario.close
--- funcao_nutricao.py
+++ funcao_nutricao.py
@@ -5,110 +5,74 @@ Calculará o IMC segundo uma massa e altura
 '''
 
 def IMC(massa,altura):
-    '''
-    >>> IMC(90,1.88)
-    25,46
-    >>> IMC(80,1.78)
-    25,24
-    '''
-    return float("{0:.2f}".format(massa/altura**2))
-    
+  
+    return float('{0:.2f}'.format(massa/altura**2))
+
 
 '''
 Calculará o a quantidade de calorias que o usuário deveria ingerir
 '''    
-def calcula_harris(idade,massa,sexo,altura,exercicio):  
-                       # Define a função que calculará o TMB e o grau de atividade
-    ''' 
-    >>> calcula_harris(22,80,M,1.88,muito alto)
-    3680,98
-    >>> calcula_harris(40,90,M,1.88,baixo)
-    2707,04
-    >>> calcula_harris(20,55,F,1.72,muito alto)
-    2416,38
-    >>> calcula_harris(40,62,F,1.72,baixo)
-    1947,00
-
-    ''' 
-    altura = altura*100     
+def calcula_harris(idade,massa,sexo,altura,exercicio):                         # Define a função que calculará o TMB e o grau de atividade
+    '''
+    >>> calcula_harris(18,65,'M',1.55,'baixo')
+    1188.28 
+    >>> calcula_harris(18,70,'M',1.70,'intermediario')
+    144.48 
+    >>> calcula_harris(18,80,'M',1.85,'muito alto')
+    2026.62 
+    >>> calcula_harris(60,65,'M',1.55,'minimo')
+    749.76 
+    >>> calcula_harris(60,70,'M',1.70,'intermediario')
+    1073.41 
+    >>> calcula_harris(60,80,'M',1.85,'alto')
+    1426.99 
+    >>> calcula_harris(26,65,'M',1.55,'baixo')
+    1125.58 
+    >>> calcula_harris(26,70,'M',1.70,'intermediario')
+    1373.80 
+    >>> calcula_harris(26,80,'M',1.85,'alto')
+    1761.29 
+    >>> calcula_harris(18,60,'F',1.55,'baixo')
+    1487.48 
+    >>> calcula_harris(18,70,'F',1.70,'muito alto')
+    2231.11 
+    >>> calcula_harris(18,75,'F',1.80,'intermediario')
+    1891.90 
+    >>> calcula_harris(60,60,'F',1.55,'alto')
+    2177.65 
+    >>> calcula_harris(60,70,'F',1.70,'minimo')
+    1862.95 
+    >>> calcula_harris(60,75,'F',1.80,'baixo')
+    1926.62 
+    >>> calculla_harris(26,60,'F',1.55,'alto')
+    1925.45 
+    >>> calculla_harris(26,70,'F',1.70,'intermediario')
+    1873.44 
+    >>> calculla_harris(26,75,'F',1.80,'muito alto')
+    2384.46 
+    '''    
     if sexo == 'F':
         if exercicio == 'minimo':
-            return float("{0:.2f}".format(447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.2)
+            return float('{0:.2f}'.format((447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.2))
         elif exercicio == 'baixo':
-            return float("{0:.2f}".format(447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.375)
+            return float('{0:.2f}'.format((447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.375))
         elif exercicio == 'alto':
-            return float("{0:.2f}".format(447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.725)
+            return float('{0:.2f}'.format((447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.725))
         elif exercicio == 'muito alto':
-            return float("{0:.2f}".format(447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.9)
+            return float('{0:.2f}'.format((447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.9))
         else:
-            return float("{0:.2f}".format(447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.55)
+            return float('{0:.2f}'.format((447.6+(9.2*massa)+(3.1*altura)-(4.3*idade))*1.55))
     if sexo == 'M':
         if exercicio == 'minimo':
-            return float("{0:.2f}".format(88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.2)
+            return float('{0:.2f}'.format((88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.2))
         elif exercicio == 'baixo':
-            return float("{0:.2f}".format(88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.375)
+            return float('{0:.2f}'.format((88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.375))
         elif exercicio == 'alto':
-            return float("{0:.2f}".format(88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.725)
+            return float('{0:.2f}'.format((88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.725))
         elif exercicio == 'muito alto':
-            return float("{0:.2f}".format(88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.9)
+            return float('{0:.2f}'.format((88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.9))
         else:
-            return float("{0:.2f}".format(88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.55)
+            return float('{0:.2f}'.format((88.36+(13.4*massa)+(4.8*altura)-(5.7*idade))*1.55))
     else:
         print('Este programa não se aplica a você, seja lá o que você for')
-
-        
-'''
-Plotará os gráficos das calorias
-'''
-
-def grafico_calorias(dias,cal_ingerida,cal_recomendada):
-    import matplotlib.pyplot as plt
-    import numpy as np
-    
-    calorias_consumidas = []
-    for a in dias:
-        calorias_consumidas.append(cal_ingerida)
-     
-    todos_elementos = calorias_consumidas + cal_recomendada
-     
-    ax = plt.subplot(111)
-    ax.bar(np.arange(0,len(dias))+0.8, calorias_consumidas,width=0.2,color='b')
-    ax.bar(np.arange(0,len(dias))+1.0, cal_recomendada,width=0.2,color='r')
-    ax.set_xticks(np.arange(len(dias))+1)
-    ax.set_xticklabels(dias)
-    plt.axis([0,len(dias)+1,0,max(todos_elementos)+200])
-    legenda="Calorias ingeridas(Kcal)","Calorias recomendadas(Kcal)"
-    plt.legend(legenda,bbox_to_anchor=(1.0, -0.15),ncol=2,fancybox=True, shadow=True)
-    plt.show()   
-
-''' 
-Plotará os gráficos do consumo dos outros nutrientes
-'''
-def grafico_nutricional(dias,prot_ingerida,carb_ingerido,gord_ingerida):
-    import matplotlib.pyplot as plt
-    import numpy as np
-    
-    proteinas = list()
-    carboidratos = list()
-    gorduras = list()
-    for a in dias:
-        proteinas.append(prot_ingerida)
-        carboidratos.append(carb_ingerido)
-        gorduras.append(gord_ingerida)        
-
-    todos_elementos = proteinas+carboidratos+gorduras    
-    
-    y = proteinas
-    z = carboidratos
-    k = gorduras
-    
-    ax = plt.subplot(111)
-    ax.bar(np.arange(0,len(dias))+0.7, y,width=0.2,color='b')
-    ax.bar(np.arange(0,len(dias))+0.9, z,width=0.2,color='g')
-    ax.bar(np.arange(0,len(dias))+1.1, k,width=0.2,color='r')
-    ax.set_xticks(np.arange(len(dias))+1)
-    ax.set_xticklabels(dias)
-    plt.axis([0,len(dias)+1,0,max(todos_elementos)+5])
-    legendas="Proteínas","Carboidratos","Gorduras"
-    plt.legend(legendas,bbox_to_anchor=(1.0, -0.15),ncol=2,fancybox=True, shadow=True)
-    plt.show()
+           
